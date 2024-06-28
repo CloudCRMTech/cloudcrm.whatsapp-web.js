db3e434bb592dbcaf7206a23fe267bc6d6348e42
rodrigo-cloudcrm <83589629+rodrigo-cloudcrm@users.noreply.github.com>
2024-06-28 12:01:39 -0300

    Update index.ts

--- src/wpp/index.ts
+++ src/wpp/index.ts
@@ -11,7 +11,7 @@ const WppClient = new Client({
     }),
     webVersionCache: {
         type: 'remote',
-        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html`,
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html`,
     }
 })
 
